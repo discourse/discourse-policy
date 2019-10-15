fbd7cb99def0e46e52956f883eb940caf7b94f48
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2019-10-15 11:11:46 +1100

    FIX: migrate custom fields data to logs

---
+++ db/post_migrate/20191014224419_migrate_custom_field_to_logs.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class MigrateCustomFieldToLogs < ActiveRecord::Migration[5.2]
+  def up
+    execute(<<~SQL)
+    INSERT INTO user_policy_logs(post_policy_id, user_id, version, accepted_at, created_at, updated_at)
+    SELECT post_policies.id, post_custom_fields.value::INTEGER, post_policies.version, post_custom_fields.created_at, post_custom_fields.created_at, post_custom_fields.updated_at
+    FROM post_custom_fields
+    INNER JOIN post_policies ON post_policies.post_id = post_custom_fields.post_id
+    WHERE post_custom_fields.name = 'PolicyAcceptedBy'
+    SQL
+
+    PostCustomField.where(name: 'PolicyAcceptedBy').delete_all
+  end
+end
