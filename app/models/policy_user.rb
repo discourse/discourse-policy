c9bbc2e53f5ec26838f7c8b2f50cd73fc20633ce
Bianca Nenciu <nenciu.bianca@gmail.com>
2021-12-07 21:38:56 +0200

    Apply code suggestions

--- app/models/policy_user.rb
+++ app/models/policy_user.rb
@@ -5,10 +5,17 @@ class PolicyUser < ActiveRecord::Base
   belongs_to :user
 
   scope :accepted, -> { where.not(accepted_at: nil).where(revoked_at: nil, expired_at: nil) }
-  scope :revoked, -> { where.not(revoked_at: nil).where(expired_at: nil) }
+  scope :revoked, -> { where.not(revoked_at: nil).where(accepted_at: nil, expired_at: nil) }
   scope :with_version, ->(version) { where(version: version) }
 
   def self.add!(user, post_policy)
+    post_policy
+      .policy_users
+      .revoked
+      .with_version(post_policy.version)
+      .where(user: user)
+      .update_all(accepted_at: Time.zone.now)
+
     self.create!(
       post_policy_id: post_policy.id,
       user_id: user.id,
@@ -24,6 +31,13 @@ class PolicyUser < ActiveRecord::Base
       .with_version(post_policy.version)
       .where(user: user)
       .update_all(revoked_at: Time.zone.now)
+
+    self.create!(
+      post_policy_id: post_policy.id,
+      user_id: user.id,
+      revoked_at: Time.zone.now,
+      version: post_policy.version
+    )
   end
 end
 
