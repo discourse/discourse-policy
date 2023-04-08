8c44f7fae1ac6a8046f4b6b159edb66df78c9142
Jarek Radosz <jradosz@gmail.com>
2023-04-08 09:03:54 +0200

    DEV: Fix `{{input}}` deprecations (#98)

--- assets/javascripts/discourse/components/policy-builder-form.js
+++ assets/javascripts/discourse/components/policy-builder-form.js
@@ -1,7 +1,18 @@
 import Component from "@ember/component";
+import { action } from "@ember/object";
 
 export default class PolicyBuilderForm extends Component {
   tagName = null;
   policy = null;
   onChange = null;
+
+  @action
+  changeValue(field, event) {
+    this.onChange(field, event.target.value);
+  }
+
+  @action
+  changeBoolValue(field, event) {
+    this.onChange(field, event.target.checked);
+  }
 }
