650114a6aca76d97596b29049fe9b4c0f6e47630
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2023-10-23 08:14:12 +0800

    DEV: Switch to new addComposerToolbarPopupMenuOption plugin API (#111)
    Why this change?
    
    `api.addToolbarPopupMenuOptionsCallback` has been deprecated in https://github.com/discourse/discourse/commit/913fd3a7b392b492f6344102577960a6eada00ce

--- assets/javascripts/initializers/add-policy-builder.js
+++ assets/javascripts/initializers/add-policy-builder.js
@@ -5,34 +5,25 @@ function initializePolicyBuilder(api, container) {
   const currentUser = api.getCurrentUser();
   const siteSettings = container.lookup("site-settings:main");
 
-  api.addToolbarPopupMenuOptionsCallback(() => {
-    if (!currentUser) {
-      return;
-    }
-
-    if (!siteSettings.policy_restrict_to_staff_posts || currentUser.staff) {
-      return {
-        label: "discourse_policy.builder.attach",
-        id: "insertPolicy",
-        group: "insertions",
-        icon: "file-signature",
-        action: "insertPolicy",
-      };
-    }
-  });
-
-  api.modifyClass("controller:composer", {
-    pluginId: "discourse-policy",
-    actions: {
-      insertPolicy() {
+  if (currentUser) {
+    api.addComposerToolbarPopupMenuOption({
+      label: "discourse_policy.builder.attach",
+      icon: "file-signature",
+      group: "insertions",
+      action: (toolbarEvent) => {
         showModal("policy-builder").setProperties({
           insertMode: true,
           post: null,
-          toolbarEvent: this.toolbarEvent,
+          toolbarEvent,
         });
       },
-    },
-  });
+      condition: () => {
+        return (
+          !siteSettings.policy_restrict_to_staff_posts || currentUser.staff
+        );
+      },
+    });
+  }
 }
 
 export default {
@@ -41,7 +32,7 @@ export default {
   initialize(container) {
     const siteSettings = container.lookup("site-settings:main");
     if (siteSettings.policy_enabled) {
-      withPluginApi("0.8.7", (api) => initializePolicyBuilder(api, container));
+      withPluginApi("1.15.0", (api) => initializePolicyBuilder(api, container));
     }
   },
 };
