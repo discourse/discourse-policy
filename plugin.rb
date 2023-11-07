7c71b06599746a18a441b5cd872555b00eff976e
Martin Brennan <mjrbrennan@gmail.com>
2023-11-08 09:14:29 +1000

    Update plugin.rb metadata (#114)
    Co-authored-by: AlexDev <104522507+alexdevlaeminck@users.noreply.github.com>

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 # name: discourse-policy
-# about: Apply policies to Discourse topics
+# about: Gives the ability to confirm your users have seen or done something, with optional reminders.
+# meta_topic_id: 88557
 # version: 0.1
 # authors: Sam Saffron
 # url: https://github.com/discourse/discourse-policy
