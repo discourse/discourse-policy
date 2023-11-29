e3ed3b4a4285ad546cfc0a090be248e65c33f409
Jarek Radosz <jradosz@gmail.com>
2023-11-29 19:10:03 +0100

    DEV: Update linting

--- assets/javascripts/discourse/components/policy-group-input.js
+++ assets/javascripts/discourse/components/policy-group-input.js
@@ -1,6 +1,6 @@
 import Component from "@glimmer/component";
-import { inject as service } from "@ember/service";
 import { action, computed } from "@ember/object";
+import { inject as service } from "@ember/service";
 
 export default class PolicyGroupInput extends Component {
   @service site;
